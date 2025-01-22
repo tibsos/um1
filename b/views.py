6a8fd179c6a18f78c3fa9d6471a638d9e6f54ce5
tibsos <kosyakovsn@gmail.com>
2025-01-22 22:05:05 +0300

    x

--- b/views.py
+++ b/views.py
@@ -3,13 +3,24 @@ from .models import *
 from django.http import JsonResponse
 
 def landing(request):
+
     subjects = Subject.objects.all()
+
     return render(request, 'landing.html', {'subjects': subjects})
 
 def subject(request, subject_slug):
+
     subject = get_object_or_404(Subject, slug=subject_slug)
     fields = Field.objects.filter(subject=subject)
-    return render(request, 'subject.html', {'subject': subject, 'fields': fields})
+
+    c = {
+
+        'subject': subject,
+        'fields': fields,
+
+    }
+
+    return render(request, 'subject.html', c)
 
 def field(request, subject_slug, field_slug):
     
@@ -28,6 +39,7 @@ def field(request, subject_slug, field_slug):
     return render(request, 'field.html', c)
 
 def topic(request, subject_slug, field_slug, topic_slug):
+
     subject = get_object_or_404(Subject, slug=subject_slug)
     field = get_object_or_404(Field, slug=field_slug, subject=subject)
     topic = get_object_or_404(Topic, slug=topic_slug, subject=subject)
@@ -52,21 +64,3 @@ def lesson(request, subject_slug, field_slug, topic_slug, lesson_slug):
     lesson = get_object_or_404(Lesson, slug=lesson_slug, topic=topic)
     
     return render(request, 'lesson.html', {'subject': subject, 'field': field, 'topic': topic, 'lesson': lesson})
-
-def ajax_search(request):
-    query = request.GET.get('q', '').strip()
-    if not query:
-        return JsonResponse({'results': []})
-    
-    subjects = Subject.objects.filter(title__icontains=query).values('title', 'slug')
-    fields = Field.objects.filter(title__icontains=query).values('title', 'slug')
-    topics = Topic.objects.filter(title__icontains=query).values('title', 'slug')
-    lessons = Lesson.objects.filter(title__icontains=query).values('title', 'slug')
-    
-    results = {
-        'subjects': list(subjects),
-        'fields': list(fields),
-        'topics': list(topics),
-        'lessons': list(lessons),
-    }
-    return JsonResponse({'results': results})

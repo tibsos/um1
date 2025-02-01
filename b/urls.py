4dc8856ba3093c1849e39331f40b8c04ee33a1a7
mikhail <mishasvetov33@gmail.com>
2025-02-02 01:51:00 +0300

    LEssonHtml

--- b/urls.py
+++ b/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('<slug:subject_slug>/<slug:field_slug>/', field, name='field'),
     path('<slug:subject_slug>/<slug:field_slug>/<slug:topic_slug>/', topic, name='topic'),
     path('<slug:subject_slug>/<slug:field_slug>/<slug:topic_slug>/<slug:lesson_slug>/', lesson, name='lesson'),
-    path('ajax_search/', ajax_search, name='ajax_search'),
+    path('ajax_search', ajax_search, name='ajax_search'),
 ]

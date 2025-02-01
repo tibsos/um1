74a06c2c953ec8d920122863da9ed0781dab5149
tibsos <kosyakovsn@gmail.com>
2025-02-02 00:49:51 +0300

    x

--- b/urls.py
+++ b/urls.py
@@ -5,8 +5,8 @@ from .views import *
 urlpatterns = [
     path('', landing, name='landing'),
     path('<slug:subject_slug>/', subject, name='subject'),
-    path('<slug:subject_slug>/fields/<slug:field_slug>/', field, name='field'),
-    path('<slug:subject_slug>/fields/<slug:field_slug>/topics/<slug:topic_slug>/', topic, name='topic'),
-    path('<slug:subject_slug>/fields/<slug:field_slug>/topics/<slug:topic_slug>/lessons/<slug:lesson_slug>/', lesson, name='lesson'),
+    path('<slug:subject_slug>/<slug:field_slug>/', field, name='field'),
+    path('<slug:subject_slug>/<slug:field_slug>/<slug:topic_slug>/', topic, name='topic'),
+    path('<slug:subject_slug>/<slug:field_slug>/<slug:topic_slug>/<slug:lesson_slug>/', lesson, name='lesson'),
     path('ajax_search/', ajax_search, name='ajax_search'),
 ]
